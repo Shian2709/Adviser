9ff82db47c43fcb05bceeae233aa796a564476f9
74929911@certus.edu.pe <74929911@certus.edu.pe>
2021-10-05 06:02:35 -0500

    commit

--- main.js
+++ main.js
@@ -5,19 +5,15 @@ const mostrar=()=> {
     let $text = document.getElementById("date");
     const totalTrabajador = $date_one * $date_two;
     const porcentajeSueldo = Math.round((100 * totalTrabajador) / $date_three);
-    if ($date_three > totalTrabajador) {
-        var x = $text.textContent = `Los trabajadores ocupan el ${porcentajeSueldo}% de tu ganancia mensual que es de ${$date_three}`;
-        $text.setAttribute("readonly", "");
-    }
-    else {
-        var x = $text.textContent = `No se puede hacer el cálculo porque la cantidad de trabajadores y su ganancia es mayor que el monto mensual`
-        $text.style.color = "red";
-        $text.setAttribute("readonly", "");
+    ($date_three > totalTrabajador) ? (x = $text.textContent = `Los trabajadores ocupan el ${porcentajeSueldo}% de tu ganancia mensual que es de ${$date_three}`,
+        $text.setAttribute("readonly", "")) : (x = $text.textContent = `No se puede hacer el cálculo porque la cantidad de trabajadores y su ganancia es mayor que el monto mensual`,
+        $text.style.color = "red",
+        $text.setAttribute("readonly", ""));
     }
     document.getElementById("reproducir").addEventListener("click", () => {
         decir(x);
     });
     function decir(x) {
         speechSynthesis.speak(new SpeechSynthesisUtterance(x));
-    }
 }
+
